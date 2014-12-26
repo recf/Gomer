64d4abe82f3a84dedc608c5794d032a8eb9cff96
Aaron Dorheim-Davis <aaron.bdd@gmail.com>
2014-12-26 14:11:24 -0800

    PileGame to own file, enable package restore,

--- Gomer.Cli/AddCommand.cs
+++ Gomer.Cli/AddCommand.cs
@@ -13,15 +13,32 @@ namespace Gomer.Cli
         {
             IsCommand("add", "Add a pile game.");
 
+            OnPileDate = DateTime.Today;
+            Priority = 2;
+            Hours = 10;
+            Genres = new List<string>();
+            
+            HasOption("d|on-pile-date=", string.Format("{{DATE}} acquired. (default: {0:yyyy-MM-dd})", OnPileDate), (DateTime v) => OnPileDate = v);
+            HasOption("p|priority=", "{PRIORITY} of the game.", (int v) => Priority = v);
+            HasOption("h|hours=", "Estimated {HOURS} to complete.", (int v) => Hours = v);
+            HasOption("g|genre=", "{GENRE} that the game belongs to. Can be specified multiple times.", v => Genres.Add(v));
+
             HasAdditionalArguments(2, "<name> <platform>");
         }
+
+        public IList<string> Genres { get; set; }
+
+        public int Hours { get; set; }
+
+        public int Priority { get; set; }
+        
+        public DateTime OnPileDate { get; set; }
 
         #region Overrides of ConsoleCommand
 
         public override int Run(string[] remainingArguments)
         {
             return 0;
-            throw new NotImplementedException();
         }
 
         #endregion
--- Gomer.Core/PIle.cs
+++ Gomer.Core/PIle.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,29 +11,5 @@ namespace Gomer.Core
     {
         [JsonProperty("games", Required = Required.Always)]
         public IList<PileGame> Games { get; set; }
-    }
-
-    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
-    public class PileGame
-    {
-        [JsonProperty("name", Required = Required.Always)]
-        public string  Name { get; set; }
-
-        [JsonProperty("platform", Required = Required.Always)]
-        public string Platform { get; set; }
-
-        [JsonProperty("on_pile_date", Required = Required.Always)]
-        public DateTime OnPileDate { get; set; }
-
-        [JsonProperty("priority", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        [DefaultValue(2)]
-        public int Priority { get; set; }
-
-        [JsonProperty("hours", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        [DefaultValue(10)]
-        public int EstimatedHours { get; set; }
-
-        [JsonProperty("genres", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        public IList<string> Genres { get; set; }
     }
 }

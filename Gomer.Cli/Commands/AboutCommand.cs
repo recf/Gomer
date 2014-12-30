1bc3588b38e93afdb099dd9040ae1753f4e5b376
Aaron Dorheim-Davis <aaron.bdd@gmail.com>
2014-12-29 21:37:05 -0800

    AboutCommand

---
+++ Gomer.Cli/Commands/AboutCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ManyConsole;
+
+namespace Gomer.Cli.Commands
+{
+    public class AboutCommand : ConsoleCommand
+    {
+        public AboutCommand()
+        {
+            IsCommand("about", "Show information about the app.");
+        }
+
+        #region Overrides of ConsoleCommand
+
+        public override int Run(string[] remainingArguments)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var name = assembly.GetName().Name;
+
+            var versionAttr = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+            var descAttr = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>();
+
+            Console.WriteLine("{0} v{1} - {2}", name, versionAttr.InformationalVersion, descAttr.Description);
+
+            return 0;
+        }
+
+        #endregion
+    }
+}
--- Gomer.Cli/Properties/AssemblyInfo.cs
+++ Gomer.Cli/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gomer.Cli")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool for managing your pile of unfinished games.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gomer.Cli")]
